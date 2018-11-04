d43e6d5d296f4654a9b5673b05bd05f966df2f55
dlany853 <dlany853@students.bju.edu>
2018-11-04 16:44:28 -0500

    2N test project and HighScore class added

---
+++ TWON/TWON/Model/HighScore.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace TWON
+{
+	class HighScore
+	{
+		//Holds names of players and their high scores.
+		private Dictionary<string, int> High_Score { get; set;}
+
+		//Constructor for the Highscore class
+		public HighScore()
+		{
+
+		}
+
+		//Checks if player's score is a highscore.
+		public bool CheckIfHighScore()
+		{
+			return false;
+		}
+
+		//Adds a player's name and score to  high score file
+		public void AddToFile()
+		{
+
+		}
+
+		//Returns all the high scores and correspoinding player names from a file
+		public string GetFromFile()
+		{
+			return "Player names and high scores";
+		}
+
+		//Returns the best score from a file to be displayed on the leader board. 
+		public string BestScore()
+		{
+			return "The best score";
+		}
+	}
+}
