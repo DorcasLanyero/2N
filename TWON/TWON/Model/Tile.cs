d79a2c5910350c6a8cfca038305dda22586339a3
T145 <T145@protonmail.com>
2018-11-05 19:16:15 -0500

    Added tile color values

--- TWON/TWON/Model/Tile.cs
+++ TWON/TWON/Model/Tile.cs
@@ -10,9 +10,28 @@ namespace TWON
 		{
 			switch (Value)
 			{
-				// TODO: Get other colors for Value's value
+				case 4:
+					return Color.FromHex("#ede0c8");
+				case 8:
+					return Color.FromHex("#f2b179");
+				case 16:
+					return Color.FromHex("#f59563");
+				case 32:
+					return Color.FromHex("#f67c5f");
+				case 64:
+					return Color.FromHex("#f65e3b");
+				case 128:
+					return Color.FromHex("#edcf72");
+				case 256:
+					return Color.FromHex("#edcc61");
+				case 512:
+					return Color.FromHex("#edc850");
+				case 1024:
+					return Color.FromHex("#edc53f");
+				case 2048:
+					return Color.FromHex("#edc22e");
 				default:
-					return Color.Maroon;
+					return Color.FromHex("#eee4da");
 			}
 		}
 	}
