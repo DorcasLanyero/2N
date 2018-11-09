9f9c2112c796c4bee85237fdeb79b490b2bab980
mmill452 <mmill452@students.bju.edu>
2018-11-09 14:40:34 -0500

    Added button to navigate to GamePage

--- TWON/TWON/View/Pages/MainPage.cs
+++ TWON/TWON/View/Pages/MainPage.cs
@@ -1,15 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TWON.View.Pages;
 using Xamarin.Forms;
 
 namespace TWON
 {
     public class MainPage : ContentPage
     {
+		StackLayout MainLayout = new StackLayout();
 		public MainPage()
 		{
-			// TODO: Add page stuff here
+
+			Button GameBtn = new Button();
+			GameBtn.Text = "Start Game";
+			GameBtn.Clicked += GameBtn_Clicked;
+			MainLayout.Children.Add(GameBtn);
+
+			// https://stackoverflow.com/questions/24929977/add-a-child-to-the-parent-on-button-click-xamarin-forms
+			Content = MainLayout; 
 		}
-    }
+
+		private void GameBtn_Clicked(object sender, EventArgs e)
+		{
+			App.Current.MainPage = new GamePage();
+		}
+	}
 }
